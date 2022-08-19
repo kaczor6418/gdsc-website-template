d40d6895ffe343718e077c954e5eb04bc2bd323b
Krzysztof Kaczyński <k.kaczynski97@gmail.com>
2022-08-20 01:54:42 +0200

    ref(typescript): conver rest independed js files to ts
    Related work item: GH-3

--- src/common/CONSTANTS.ts
+++ src/common/CONSTANTS.ts
@@ -1,3 +1,4 @@
 export namespace CONSTANTS {
   export const INITIAL_COMPONENT_ID = 0;
+  export const DEFAULT_RIPPLE_EFFECT_TIME = 500;
 }
--- src/common/types.ts
+++ src/common/types.ts
@@ -10,3 +10,8 @@ export interface ContactMedia {
   iconId: SocialMediaType;
   url: string;
 }
+
+export interface Size {
+  height: number;
+  width: number;
+}
